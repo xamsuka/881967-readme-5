9983db44483d4a4c004e43f678f13e1cc1fb208b
Vladislav Kozlovsky <kozlovsk.v@yandex.ru>
2023-12-10 21:25:35 +0600

    feat: deletes unnecessary files

--- project/libs/shared/types/src/lib/libs-shared-types.spec.ts
+++
@@ -1,7 +0,0 @@
-import { libsSharedTypes } from './libs-shared-types';
-
-describe('libsSharedTypes', () => {
-  it('should work', () => {
-    expect(libsSharedTypes()).toEqual('libs-shared-types');
-  });
-});
--- project/libs/shared/types/src/lib/libs-shared-types.ts
+++
@@ -1,3 +0,0 @@
-export function libsSharedTypes(): string {
-  return 'libs-shared-types';
-}
