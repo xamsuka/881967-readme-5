7b09e95b3994b4192bb28bda0704db6d7ef576ee
Vladislav Kozlovsky <kozlovsk.v@yandex.ru>
2023-12-10 21:12:17 +0600

    feat: add general library libs/shared/types

---
+++ project/libs/shared/types/src/lib/libs-shared-types.spec.ts
@@ -0,0 +1,7 @@
+import { libsSharedTypes } from './libs-shared-types';
+
+describe('libsSharedTypes', () => {
+  it('should work', () => {
+    expect(libsSharedTypes()).toEqual('libs-shared-types');
+  });
+});
---
+++ project/libs/shared/types/src/lib/libs-shared-types.ts
@@ -0,0 +1,3 @@
+export function libsSharedTypes(): string {
+  return 'libs-shared-types';
+}
