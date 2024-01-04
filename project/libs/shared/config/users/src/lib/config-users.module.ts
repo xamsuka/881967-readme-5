22523592ca99e07cf4f6a9b42684965cedaf93a5
Vladislav Kozlovsky <kozlovsk.v@yandex.ru>
2024-01-04 15:10:23 +0600

    feat: add app and mongo configuration

--- project/libs/shared/config/users/src/lib/config-users.module.ts
+++ project/libs/shared/config/users/src/lib/config-users.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import appConfiguration from './configs/app.config';
+import mongoConfiguration from './configs/mongo.config';
 
 const ENV_USERS_FILE_PATH = 'apps/users/users.env';
 
@@ -8,8 +10,7 @@ const ENV_USERS_FILE_PATH = 'apps/users/users.env';
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      // TODO: Передать список конфигураций для загрузки
-      load: [],
+      load: [appConfiguration, mongoConfiguration],
       envFilePath: ENV_USERS_FILE_PATH,
     }),
   ],
