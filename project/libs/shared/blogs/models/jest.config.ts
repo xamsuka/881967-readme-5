f84e8d7fbd800a2c20e6f86ca28af58f0fd9b4a9
Vladislav Kozlovsky <vkozlovskiy@phoenixit.ru>
2024-12-22 13:09:38 +0600

    feat: add blogs library

---
+++ project/libs/shared/blogs/models/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'libs/shared/blogs/models',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/shared/blogs/models',
+};
