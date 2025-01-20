d9145105dd3b9fdbfd310d2325c16b682e2672f1
Vladislav Kozlovsky <vkozlovskiy@phoenixit.ru>
2025-01-20 17:31:17 +0600

    feat: add validation

--- project/apps/blogs/src/app/blogs/dto/create-blog.dto.ts
+++ project/apps/blogs/src/app/blogs/dto/create-blog.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
+import { IsString, IsUUID } from 'class-validator';
 
 export class CreateBlogDto {
   @ApiProperty({
     description: 'User email',
     example: 'example@mail.ru',
   })
+  @IsString()
   @Expose()
   name: string;
 
@@ -13,6 +15,7 @@ export class CreateBlogDto {
     description: 'User unique uuid key',
     example: '3422b448-2460-4fd2-9183-8000de6f8343',
   })
+  @IsUUID()
   @Expose()
   userId: string;
 }
--- project/apps/blogs/src/app/blogs/dto/update-blog.dto.ts
+++ project/apps/blogs/src/app/blogs/dto/update-blog.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
+import { IsString, IsUUID } from 'class-validator';
 
 export class UpdateBlogDto {
   @ApiProperty({
     description: 'User email',
     example: 'example@mail.ru',
   })
+  @IsString()
   @Expose()
   name: string;
 
+  @IsUUID()
   @Exclude()
   userId: string;
 }
