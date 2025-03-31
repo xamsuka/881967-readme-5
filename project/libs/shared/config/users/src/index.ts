903bb0180e46f7e930c758c18e9c609c7d16ef8f
Vladislav Kozlovsky <vkozlovskiy@phoenixit.ru>
2025-03-31 14:33:29 +0600

    feat: add getJwtOptions

--- project/libs/shared/config/users/src/index.ts
+++ project/libs/shared/config/users/src/index.ts
@@ -2,3 +2,4 @@ export { ConfigUsersModule } from './lib/config-users.module';
 export { ApplicationConfig } from './lib/configs/app.config';
 export { MongoConfig } from './lib/configs/mongo.config';
 export { usersConfigNamespace } from './lib/configs/users.config.namespace';
+export { getJwtOptions } from './lib/configs/get-jwt-options';
